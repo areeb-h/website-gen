d72cacb64876d409925b62b2b300d7ec09f68bc1
Areeb <areeb.hw@gmail.com>
2024-11-20 03:06:55 +0500

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,12 @@
 'use client'
 
 import { motion } from 'framer-motion'
-import { Instagram, Twitter, LinkedinIcon as LinkedIn, Facebook } from 'lucide-react'
+import { Instagram, LinkedinIcon as LinkedIn, Facebook, X } from 'lucide-react'
 
 const socialLinks = [
     { href: 'https://facebook.com/ocelopticnetwork', icon: Facebook, label: 'Facebook', color: 'text-[#1877F2]' },
     { href: 'https://instagram.com/ocelopticnetwork', icon: Instagram, label: 'Instagram', color: 'text-[#E1306C]' },
-    { href: 'https://twitter.com/ocelopticnetwork', icon: Twitter, label: 'Twitter', color: 'text-[#1DA1F2]' },
+    { href: 'https://twitter.com/ocelopticnetwork', icon: X, label: 'Twitter', color: 'text-[#000000]' },
     { href: 'https://linkedin.com/company/ocelopticnetwork', icon: LinkedIn, label: 'LinkedIn', color: 'text-[#0A66C2]' },
 ]
 
@@ -14,11 +14,10 @@ const Footer = () => {
     return (
         <footer className="bg-gray-50 border-t border-orange-100">
             <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+                <div className="grid grid-cols-2 /md:grid-cols-2 gap-8">
                     {/* Contact Details */}
-                    <div className="space-y-4">
-                        <div className="flex items-center space-x-3">
-                            {/* <ArrowRight className="h-5 w-5 text-orange-500" /> */}
+                    <div className="flex flex-col space-y-4 /text-center md:text-left">
+                        <div>
                             <a
                                 href="mailto:connect@ocelopticnetwork.com"
                                 className="text-base md:text-xl border-b-4 font-bold text-orange-600 hover:text-orange-700 transition-colors"
@@ -26,8 +25,7 @@ const Footer = () => {
                                 connect@ocelopticnetwork.com
                             </a>
                         </div>
-                        <div className="flex items-center space-x-3">
-                            {/* <Phone className="h-5 w-5 text-orange-500" /> */}
+                        <div>
                             <a
                                 href="tel:+960925925404"
                                 className="text-base md:text-lg text-gray-700 hover:text-orange-600 transition-colors"
@@ -35,19 +33,21 @@ const Footer = () => {
                                 +960 925 925 4
                             </a>
                         </div>
-                        <div className="flex items-start space-x-3">
-                            {/* <MapPin className="h-5 w-5 text-orange-500 mt-1" /> */}
+                        <div>
                             <span className="text-base text-gray-700">
                                 Ma. Perihelion, Ameenee Magu,
                                 <br />
                                 Male&apos;, Maldives
                             </span>
                         </div>
+
+
+
                     </div>
 
                     {/* Social Links */}
-                    <div className="flex justify-start md:justify-end items-start">
-                        <div className="grid grid-cols-2 gap-4">
+                    <div className="flex justify-end md:justify-end items-start">
+                        <div className="grid md:grid-cols-2 grid-cols-1 gap-4 p-4 bg-gray-100 w-fit rounded-full">
                             {socialLinks.map(({ href, icon: Icon, label, color }) => (
                                 <motion.a
                                     key={label}
