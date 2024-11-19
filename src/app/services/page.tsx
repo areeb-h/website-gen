ca343beb10f659807dd57f066a7b50eac423fd35
Areeb <areeb.hw@gmail.com>
2024-11-19 22:01:44 +0500

    new

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import Image from 'next/image';
 import { motion } from 'framer-motion';
 import {
-    PieChart, Globe, Target,
+    PieChart, Globe,
     BarChart2, Award, ArrowRight, CheckCircle
 } from 'lucide-react';
 
